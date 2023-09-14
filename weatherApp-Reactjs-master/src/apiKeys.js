b3f3e55836c7a233602416d2bce2485d7546479e
unknown <susilkumarbehera91@gmail.com>
2023-09-14 22:54:30 +0530

    LiveWeatherApp

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "35c6c9da1d36b8b176d98fece9679e16",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
